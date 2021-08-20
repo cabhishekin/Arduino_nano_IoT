8f5a1b15935fcf6a72d4d9e1a8e3727218ec2b1c
cabhishekin <67386763+cabhishekin@users.noreply.github.com>
2021-08-20 10:49:22 -0400

    Add files via upload

---
+++ Arduino_Source_Code .cpp
@@ -0,0 +1,36 @@
+/*
+
+*/
+
+#include <Servo.h> 
+
+Servo myservo; 
+
+int pos = 0; 
+  
+
+void setup() { 
+
+    myservo.attach(9); 
+
+} 
+
+
+void loop() { 
+
+    myservo.write(0); 
+
+    delay(100); 
+
+    myservo.write(90); 
+
+    delay(100); 
+
+    myservo.write(180); 
+
+//    delay(2000); 
+
+
+    delay(100);  
+
+} 
